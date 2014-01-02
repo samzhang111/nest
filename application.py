be0c36c8bf164c4f98e476a28fa3cf3bb30b4ea6
Sam <unclesaamm@gmail.com>
2014-01-01 21:33:04 -0500

    fixed typo

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://ebroot:jowran8I@aae5w8f5ejqw45.csi1cndzkahd.us-east-1.rds.amazonaws.com'
 db = SQLAlchemy(application)
 
-@app.route("/")
+@application.route("/")
 def hello():
     return "Hello World!"
 
